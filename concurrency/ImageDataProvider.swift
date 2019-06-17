bf39d0608a0a66c9405d7dfeddbdfe92bccb0913
STANISLAV <minolin27@gmail.com>
2019-06-18 00:05:35 +0300

    push project to github

---
+++ concurrency/ImageDataProvider.swift
@@ -0,0 +1,10 @@
+
+
+import Foundation
+import UIKit
+
+protocol ImageDataProvider {
+  var image: UIImage? { get }
+  
+  
+}
---
+++ concurrency/PhotoCell.swift
@@ -0,0 +1,24 @@
+
+
+import UIKit
+
+final class PhotoCell: UITableViewCell {
+  @IBOutlet private weak var nasaImageView: UIImageView!
+  @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+
+  var isLoading: Bool {
+    get { return activityIndicator.isAnimating }
+    set {
+      if newValue {
+        activityIndicator.startAnimating()
+      } else {
+        activityIndicator.stopAnimating()
+      }
+    }
+  }
+
+  func display(image: UIImage?) {
+    nasaImageView.image = image
+  }
+}
+
